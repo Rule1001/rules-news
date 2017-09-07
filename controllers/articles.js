25f2a6303c7a2a83105347ab5e2d760bce0bf03c
Rule1001 <hello@rule1001.com>
2017-09-07 13:41:43 +0100

    added vote article function. tests passing

--- controllers/articles.js
+++ controllers/articles.js
@@ -23,3 +23,23 @@ exports.getAllArticles = (req, res) => {
         });
 };
 
+exports.voteArticle = (req, res) => {
+    let articleId = req.params.article_id;
+    let vote = req.query.vote;
+    let query;
+    Articles.findById(articleId)
+        .then((article) => {
+            if (article.votes <= 0 && vote === 'down') {
+                return res.status(200).json({ article });
+            }
+            if (vote === 'up') query = { $inc: { votes: 1 } };
+            if (vote === 'down') query = { $inc: { votes: -1 } };
+            return Articles.findByIdAndUpdate({ _id: articleId }, query, { new: true })
+                .then((article) => {
+                    res.status(200).json({ article });
+                });
+        }).catch((err) => {
+            res.status(500).json(err);
+        });
+};
+
