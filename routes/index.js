a83691283c97f61aa85b427fee1cb3c6e1962258
Rule1001 <hello@rule1001.com>
2017-09-07 13:51:32 +0100

    added path for delete comment

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,9 @@ const { getCommentsByArticle, postNewComment, deleteComment, voteComment} = requ
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Everything is fine!'));
-
 router.get('/topics', getAllTopics);
 router.get('/topics/:topic_id/articles', getArticlesByTopic);
-
 router.get('/articles', getAllArticles);
-
 router.get('/articles/:article_id/comments', getCommentsByArticle);
 
 router.post('/articles/:article_id/comments', postNewComment);
@@ -21,5 +18,6 @@ router.post('/articles/:article_id/comments', postNewComment);
 router.put('/articles/:article_id', voteArticle);
 router.put('/comments/:comment_id', voteComment);
 
+router.delete('/comments/:comment_id', deleteComment);
 
 module.exports = router;
