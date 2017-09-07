6e601d4901092f860aa3075058a802d5b42d2960
Rule1001 <hello@rule1001.com>
2017-09-07 13:59:07 +0100

    added mongoose database connection

---
+++ seed/seed.js
@@ -0,0 +1,38 @@
+var models = require('../models/models');
+var userData = require('./data/user_data.js');
+var articleData = require('./data/articles');
+var Chance = require('chance');
+var chance = new Chance();
+var _ = require('underscore');
+var async = require('async');
+var mongoose = require('mongoose');
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var moment = require('moment');
+var DBs = require('../config').DB;
+
+mongoose.connect(DBs.dev, (err) => {
+  if (!err) {
+    logger.info(`connected to database ${DBs.dev}`);
+    mongoose.connection.db.dropDatabase();
+    async.waterfall([
+      addUsers,
+      addTopics,
+      addArticles,
+      addComments,
+      addNorthcoderUser
+    ], (err) => {
+      if (err) {
+        logger.error('ERROR SEEDING :O');
+        console.log(JSON.stringify(err));
+        process.exit();
+      }
+      logger.info('DONE SEEDING!!');
+      process.exit();
+    });
+  } else {
+    logger.error('DB ERROR');
+    console.log(JSON.stringify(err));
+    process.exit();
+  }
+});
