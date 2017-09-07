121d0ef6f9f8332eb633814cea678f1c83352c15
Rule1001 <hello@rule1001.com>
2017-09-07 13:08:47 +0100

    added post new comment function. Tests passing

--- controllers/comments.js
+++ controllers/comments.js
@@ -10,3 +10,19 @@ exports.getCommentsByArticle = (req, res) => {
             res.status(500).json(err);
         });
 };
+
+exports.postNewComment = (req, res) => {
+    let articleId = req.params.article_id;
+    let comment = new Comments({
+        body: req.body.comment,
+        belongs_to: articleId
+    });
+    comment
+        .save()
+        .then((comment) => {
+            res.send({ comment: comment });
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+};
